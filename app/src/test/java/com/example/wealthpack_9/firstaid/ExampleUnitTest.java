16e0f8d8644e350e8729611d44c4ebbefb3542a6
Mohd Ali <mohd.ali@wealthpack.com>
2015-11-12 10:56:53 +0530

    Initial commit

---
+++ app/src/test/java/com/example/wealthpack_9/firstaid/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.wealthpack_9.firstaid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
