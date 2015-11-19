be4106833522f2b5fafdbc256968cf07e881b22a
Mohd Ali <mohd.ali6@yahoo.com>
2015-11-19 16:18:11 +0530

    Intent recievd in DetailActivity and basic layout for DetailActivity

--- app/src/main/java/com/example/wealthpack_9/firstaid/DetailActivity.java
+++ app/src/main/java/com/example/wealthpack_9/firstaid/DetailActivity.java
@@ -1,11 +1,12 @@
 package com.example.wealthpack_9.firstaid;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -16,15 +17,20 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent i = getIntent();
+        ArrayList<FirstAidSteps> l =  i.getParcelableArrayListExtra("accident_name");
+        Toast toast = Toast.makeText(getApplicationContext(), l.toString(), Toast.LENGTH_SHORT);
+        toast.show();
     }
 
 }
