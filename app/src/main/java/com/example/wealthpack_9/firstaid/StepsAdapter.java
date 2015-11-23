9316170cef56f468ae3b1fad0bc8104fee900bd3
Mohd Ali <mohd.ali6@yahoo.com>
2015-11-23 15:49:30 +0530

    StepsAdapter implemented

--- app/src/main/java/com/example/wealthpack_9/firstaid/StepsAdapter.java
+++ app/src/main/java/com/example/wealthpack_9/firstaid/StepsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,15 +17,53 @@ public class StepsAdapter extends ArrayAdapter<FirstAidSteps> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        FirstAidSteps steps = getItem(position);
+        ViewHolder holder;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            holder = new ViewHolder();
+            holder.stepName = (TextView) convertView.findViewById(R.id.step_heading);
+            holder.steps = (LinearLayout) convertView.findViewById(R.id.steps);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        TextView stepName = (TextView) convertView.findViewById(R.id.step_heading);
-        stepName.setText(steps.getStepName());
+        final FirstAidSteps steps = getItem(position);
+        holder.stepName.setText(steps.getStepName());
+
+        holder.steps.removeAllViews();
+        for (String step: steps.getSteps()) {
+            holder.steps.addView(createStepView(step, holder.steps));
+        }
 
         return convertView;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    private static class ViewHolder {
+        TextView stepName;
+        LinearLayout steps;
+    }
+
+    public View createStepView(String step, LinearLayout steps_layout) {
+        TextView textView = new TextView(getContext());
+        textView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        textView.setText("• " + step);
+        textView.setPadding(5, 2, 5, 2);
+        textView.setTextSize(20);
+        return textView;
     }
 }
